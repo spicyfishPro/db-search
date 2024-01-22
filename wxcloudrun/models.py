b39e66488ea468fb2463929de5b3747aa61e73d8
root <admin@example.com>
2024-01-22 12:55:49 +0800

    init

--- wxcloudrun/models.py
+++ wxcloudrun/models.py
@@ -4,14 +4,30 @@ from django.db import models
 
 
 # Create your models here.
-class Counters(models.Model):
-    id = models.AutoField
-    count = models.IntegerField(max_length=11, default=0)
-    createdAt = models.DateTimeField(default=datetime.now(), )
-    updatedAt = models.DateTimeField(default=datetime.now(),)
+
+class wheatinfo(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255)
+    母本 = models.CharField(max_length=255)
+    父本 = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
 
     class Meta:
-        db_table = 'Counters'  # 数据库表名
+        db_table = 'wheatinfo' 
+    
+
+# class Counters(models.Model):
+#     id = models.AutoField
+#     count = models.IntegerField(max_length=11, default=0)
+#     createdAt = models.DateTimeField(default=datetime.now(), )
+#     updatedAt = models.DateTimeField(default=datetime.now(),)
+
+#     def __str__(self):
+#         return self.title
+
+#     class Meta:
+#         db_table = 'wheatinfo'  # 数据库表名
--- wxcloudrun/urls.py
+++ wxcloudrun/urls.py
@@ -16,11 +16,14 @@ Including another URLconf
 
 from wxcloudrun import views
 from django.conf.urls import url
+from django.urls import path
 
 urlpatterns = (
-    # 计数器接口
-    url(r'^^api/count(/)?$', views.counter),
+    # # 计数器接口
+    # url(r'^^api/count(/)?$', views.counter),
 
-    # 获取主页
-    url(r'(/)?$', views.index),
+    # # 获取主页
+    # url(r'(/)?$', views.index),
+
+    path('search/', views.index, name='search')
 )
